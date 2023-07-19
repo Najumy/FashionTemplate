9a39e71de1f18f138cc3b445f9adab3b06b8d1a8
Namjuy <najumy1807.2k@gmail.com>
2023-07-19 09:10:52 +0700

    update web

---
+++ landing-page/index.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+  //Ajax
+  $.getJSON("../mock-data/products.json", function (data) {
+    if (data.length === 0) {
+      $(".error").text("Không có sản phẩm nào");
+      $(".btn-load-more").css({
+        display: "none",
+      });
+    } else {
+      console.log("data", data);
+      for (let index = 0; index < data.length; index++) {
+        $("#products__list").append(`
+          
+          <div class="products__item">
+          <small>Trả góp ${data[index].install}%</small>
+          <p>
+            <a href="#" class="products__img">
+              <img src=${data[index].imgUrl} alt="" />
+            </a>
+          </p>
+          <p><a href="#" class="products__title">${data[index].title}</a></p>
+          <p class="products__price">
+            <span>${data[index].price}</span>
+            <span>-${data[index].sale}</span>
+          </p>
+          <div class="products__rating">
+            <span>${data[index].rate}</span
+            ><span
+              ><i class="fa-solid fa-star" style="color: #dc18d6"></i></span
+            ><span>(${data[index]["quality"]})</span>
+          </div>
+        </div>
+          `);
+      }
+    }
+  }).fail(function (jqxhr, textStatus, error) {
+    console.log(jqxhr, textStatus, error);
+    $(".error").text(error);
+    $(".btn-load-more").css({
+        display: "none",
+      });
+  });
+});
+
+//4 method ajax: get , post, pu , delete
